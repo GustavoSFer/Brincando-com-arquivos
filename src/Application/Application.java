381210077e936a5e550e414bce23f9f166094063
GustavoSFer <gustavo.sfernander@gmail.com>
2023-10-16 22:38:12 -0300

    Criando o programa principal, instanciando o objeto e salvando em uma lista

--- src/Application/Application.java
+++ src/Application/Application.java
@@ -1,10 +1,30 @@
 package Application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import Entities.Produto;
+
 public class Application {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    List<Produto> produtos = new ArrayList<>();
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Informe quantos produtos deseja cadastrar: ");
+    int qtdProduto = sc.nextInt();
 
+    for (int i = 0; i < qtdProduto; i++) {
+      System.out.print("Informe o nome do " + (i + 1) + "ª produto: ");
+      String nome = sc.nextLine();
+      System.out.print("Informe o preço do " + (i + 1) + "ª produto: ");
+      double preco = sc.nextDouble();
+      System.out.print("Informe a quantidade em estoque do " + (i + 1) + "ª produto: ");
+      int qtdEstoque = sc.nextInt();
+
+      Produto produto = new Produto(nome, preco, qtdEstoque);
+      produtos.add(produto);
+    }
+    sc.close();
   }
 
 }
