1f47d5315ef8552596fb4a14f33c74c7d6a0dbb9
GustavoSFer <gustavo.sfernander@gmail.com>
2023-10-17 22:20:53 -0300

    Melhorando o código

--- src/Application/Application.java
+++ src/Application/Application.java
@@ -1,9 +1,7 @@
 package Application;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,35 +18,46 @@ public class Application {
     System.out.print("Informe quantos produtos deseja cadastrar: ");
     int qtdProduto = sc.nextInt();
 
-    for (int i = 0; i < qtdProduto; i++) {
-      System.out.print("Informe o nome do " + (i + 1) + "ª produto: ");
-      String nome = sc.next();
-
-      System.out.print("Informe o preço do " + (i + 1) + "ª produto: ");
-      double preco = sc.nextDouble();
-      System.out.print("Informe a quantidade em estoque do " + (i + 1) + "ª produto: ");
-      int qtdEstoque = sc.nextInt();
-
-      Produto produto = new Produto(nome, preco, qtdEstoque);
-      produtos.add(produto);
-    }
     String path = "C:\\contato\\out.csv";
     // Salvar no arquivo .csv todos os produtos digitados
-    try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
-      for (Produto produto : produtos) {
-        bw.write(produto.getNome() + "," + produto.getPreco() + "," + produto.getQtd());
-        bw.newLine();
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+    // try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+    //
+    // for (int i = 0; i < qtdProduto; i++) {
+    // System.out.print("Informe o nome do " + (i + 1) + "ª produto: ");
+    // String nome = sc.next();
+    //
+    // System.out.print("Informe o preço do " + (i + 1) + "ª produto: ");
+    // double preco = sc.nextDouble();
+    // System.out.print("Informe a quantidade em estoque do " + (i + 1) + "ª produto: ");
+    // int qtdEstoque = sc.nextInt();
+    //
+    // bw.write(nome + "," + preco + "," + qtdEstoque);
+    // bw.newLine();
+    // }
+    // } catch (IOException e) {
+    // e.printStackTrace();
+    // }
 
     // Realizando a escrita das informaçoes na saida do sistema.
     try (BufferedReader br = new BufferedReader(new FileReader(path))) {
       String line = br.readLine();
       while (line != null) {
-        System.out.println(line);
+        String[] produtoLinha = line.split(",");
+        // Colocando apenas para saber de como imprimir para ver como esta ficando
+        // System.out.println(Arrays.toString(produtoLinha));
+        String nomeProduto = produtoLinha[0];
+        Double precoProduto = Double.parseDouble(produtoLinha[1]);
+        Integer qtd = Integer.parseInt(produtoLinha[2]);
+
+        Produto produto = new Produto(nomeProduto, precoProduto, qtd);
+        produtos.add(produto);
         line = br.readLine();
+      }
+
+      System.out.println("\n== LISTA DE PRODUTOS ==\n");
+      for (Produto p : produtos) {
+        System.out.println("Produto: " + p.getNome() + ", R$: " + p.getPreco() + ", Quantidade: "
+            + p.getQtd() + " Valor total: " + p.getPriceTotal());
       }
     } catch (IOException e) {
       e.printStackTrace();
